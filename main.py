bb580aaadb26ade7781bfa0b7e8fa54c5b769bdd
root <root@Desktop-Luciano.localdomain>
2024-05-03 19:31:12 -0700

    Initial

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import TensorFlow as tf
+
